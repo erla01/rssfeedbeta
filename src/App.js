636eb13513556cf5499363e0bd7a8dc6f6c1de4a
Erik Lavfors <Erik.Lavfors@afaforsakring.se>
2020-10-08 09:36:03 +0200

    button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
