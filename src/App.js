a56955cd964efd96a695986bf89319e8aa5df05f
Erik Lavfors <Erik.Lavfors@afaforsakring.se>
2020-10-08 16:38:25 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { initDB, useIndexedDB } from 'react-indexed-db';
 import { DBConfig } from './Data/DBConfig';
 
 import './App.css';
 import Button from '@material-ui/core/Button';
-import Input from '@material-ui/core/Input';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+import Grid from '@material-ui/core/Grid';
+import { Container, TextField } from '@material-ui/core';
 
 
 initDB(DBConfig);
 
 function App() {
-  let testRss = "https://www.svt.se/nyheter/lokalt/blekinge/rss.xml";
   const [newFeed, setNewFeed] = useState("");
   // const [feeds, setFeeds] = useState([]);
   const [feedItems, setFeedItems] = useState([]);
@@ -24,58 +26,81 @@ function App() {
   const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
   const { add } = useIndexedDB('Rss');
-  // const { getAll } = useIndexedDB('Rss');
+  const { add: addRssItem, getAll: getAllRssItem } = useIndexedDB('Item');
   // const db = useIndexedDB('Rss');
 
-  function addToDb(url) {
-    add({ url: url }).then(
+  const addFeedDb = (async (url) => {
+
+    let completeFeed = await getFeed(url);
+
+    add({ url: url, feedDesc: completeFeed.description }).then(
       event => {
         console.log('ID Generated: ', event)
+        completeFeed.items.map(item => addItem(item, event))
       },
       error => {
         console.log(error);
       }
     );
-  };
+  });
 
-  // const getFeeds = () => {
-  //   getAll().then(feedsFromDB => {
-  //     setFeeds(feedsFromDB);
-  //   });
-  // }
+  const addItem = (item, RssId) => {
 
+    addRssItem({ RssId: RssId, title: item.title, guid: item.guid, isoDate: item.isoDate, link: item.link, contentSnippet: item.contentSnippet }).then(
+      event => {
+        console.log('Item ID Generated: ', event)
+      },
+      error => {
+        console.log(error);
+      }
+    );
 
+  }
 
-  const getFeed = (async () => {
+  const getFeedItems = () => {
+    getAllRssItem().then(feedItemsFromDB => {
+      setFeedItems(feedItemsFromDB);
+    });
+  }
 
-    let feed = await parser.parseURL(CORS_PROXY + testRss);
+  useEffect(
+    getFeedItems, []);
+
+  const getFeed = (async (url) => {
+
+    let feed = await parser.parseURL(CORS_PROXY + url);
     console.log(feed.title);
 
-    // feed.items.forEach(item => {
-    //   console.log(item.title + ':' + item.link)
-    // });
-    setFeedItems(feedItems.concat(feed.items));
-    console.log(feed.items);
+    return feed;
+
   });
 
 
   return (
     <>
-      <form noValidate autoComplete="off">
-        <Input placeholder="RSSFeed" onChange={(e) => setNewFeed(e.target.value)} inputProps={{ 'aria-label': 'description' }} />
-        <Button variant="contained" color="primary" onClick={() => addToDb(newFeed)}>
-          Lägg till
-      </Button>
-      </form>
-      <Button variant="contained" color="primary" onClick={getFeed}>
-        Lägg till
-      </Button>
-
+      <Container className="form-container" maxWidth="sm">
+        <form className="rss-form" noValidate autoComplete="off">
+          <Grid container spacing={2}>
+            <Grid item id="rssGridItem">
+              <TextField id="standard-full-width"
+                style={{ margin: 8 }}
+                fullWidth
+                margin="normal"
+                placeholder="RSSFeed" onChange={(e) => setNewFeed(e.target.value)} inputProps={{ 'aria-label': 'description' }} />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary" onClick={() => addFeedDb(newFeed)}>
+                Lägg till
+            </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
 
       <List>{feedItems.map(item =>
         <ListItem key={item.guid} alignItems="flex-start">
           <ListItemText
-            primary={<a href={item.link}>{item.title}</a>}
+            primary={<Link href={item.link}>{item.title}</Link>}
             secondary={
               <>
                 <Typography
@@ -91,11 +116,6 @@ function App() {
           />
         </ListItem>)}
       </List>
-
-
-      {/* {feedItems.map(item => */}
-      {/* <li key={item.guid}>{item.title}</li>)} */}
-      {/* <div>{feeds.map(feed => <span key={feed.id}>{feed.url}</span>)}</div> */}
     </>
   );
 }
--- src/Data/DBConfig.js
+++ src/Data/DBConfig.js
@@ -15,10 +15,10 @@ export const DBConfig = {
       store: 'Item',
       storeConfig: { keyPath: 'id', autoIncrement: true },
       storeSchema: [ 
-        {name: 'RssId', keyPath: 'RssId', options: {unique: true  }},
+        {name: 'RssId', keyPath: 'RssId', options: {unique: false  }},
         {name: 'title', keyPath: 'title', options: {unique: false  }},
         {name: 'isoDate', keyPath: 'isoDate', options: {unique: false  }},
-        {name: 'guid', keyPath: 'guid', options: {unique: false  }},
+        {name: 'guid', keyPath: 'guid', options: {unique: true  }},
         {name: 'contentSnippet', keyPath: 'contentSnippet', options: {unique: false  }},
         {name: 'link', keyPath: 'link', options: {unique: false  }},
       ]
